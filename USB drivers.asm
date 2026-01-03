7561a6bf4979f7cd2e6501529382661c86b7c232
Mickredset <aivliev782@gmail.com>
2026-01-03 10:02:00 +0300

    Update USB drivers.asm

--- USB drivers.asm
+++ USB drivers.asm
@@ -1,13 +1,11 @@
-; Найти USB-контроллер через PCI
 find_ehci:
     mov eax, 0x80000000
     cpuid
     ; ... поиск устройства по классу 0x0C03 (USB)
     ; Получить BAR (Base Address Register) контроллера
 
-; Настроить регистры EHCI
 init_ehci:
     mov edx, [ehci_bar]
-    mov dword [edx + 0x04], 0x00000001 ; Set USBSTS to 1 (reset)
+    mov dword [edx + 0x04], 0x00000001  ; Reset USBSTS
     ; ... ожидание сброса
-    mov dword [edx + 0x08], 0x00000001 ; Включить асинхронную очередь
+    mov dword [edx + 0x08], 0x00000001  ; Включить асинхронную очередь
