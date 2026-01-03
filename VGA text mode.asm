74a9a76b9cccae9c19dfaf462958ff384013f98d
Mickredset <aivliev782@gmail.com>
2026-01-03 09:54:36 +0300

    Create VGA text mode.asm

---
+++ VGA text mode.asm
@@ -0,0 +1,23 @@
+; Вывести символ в текстовом режиме
+putchar:
+    pusha
+    mov di, [cursor_pos]
+    mov word [0xB8000 + di*2], ax ; ax = символ + атрибут
+    inc word [cursor_pos]
+    popa
+    ret
+
+; Установить позицию курсора
+set_cursor:
+    pusha
+    mov dx, 0x3D4
+    mov al, 0x0F
+    out dx, al
+    mov al, [cursor_pos] & 0xFF
+    out dx+1, al
+    mov al, 0x0E
+    out dx, al
+    mov al, ([cursor_pos] >> 8) & 0xFF
+    out dx+1, al
+    popa
+    ret
